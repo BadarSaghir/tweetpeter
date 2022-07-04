b1f28cc858558c5503a0693ee82e58ec0da0e355
SardarBadar <dataentrybadar@gmail.com>
2022-07-04 10:23:37 +0500

    fix issues

--- sel.py
+++ sel.py
@@ -1,4 +1,5 @@
 from typing import Literal
+from html2text import element_style
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
@@ -10,13 +11,11 @@ XPATH_IMG = "/html/body/div[1]/div/div/div[2]/main/div/div/div/div[1]/div/div[2]
 
 XPATH_TIME="/html/body/div[1]/div/div/div[2]/main/div/div/div/div[1]/div/div[2]/div/div/section/div/div/div[1]/div/div/div/article/div/div/div/div[2]/div[2]/div[1]/div/div/div[1]/div/div/div[2]/div/div[3]/a/time"
 
-XPATH_CONTENT='//*[@id="react-root"]/div/div/div[2]/main/div/div/div/div/div/div[2]/div/div/section/div/div/div[1]/div/div/div/article/div/div/div/div[2]/div[2]/div[2]'
+XPATH_CONTENT="/html/body/div[1]/div/div/div[2]/main/div/div/div/div/div/div[2]/div/div/section/div/div/div[1]/div/div/div/article/div/div/div/div[2]/div[2]/div[2]/div[2]/div/span"
 
 XPATH_TWEETED_BY = "/html/body/div[1]/div/div/div[2]/main/div/div/div/div[1]/div/div[2]/div/div/section/div/div/div[1]/div/div/div/article/div/div/div/div[2]/div[2]/div[1]/div/div/div[1]/div/div/div[1]/div/a/div/div[1]/span/span"
-
 XPATH_HREF="/html/body/div[1]/div/div/div[2]/main/div/div/div/div[1]/div/div[2]/div/div/section/div/div/div[1]/div/div/div/article/div/div/div/div[2]/div[2]/div[1]/div/div/div[1]/div/div/div[1]/div/a"
 
-XPATH_CONTENT2="/html/body/div[1]/div/div/div[2]/main/div/div/div/div/div/div[2]/div/div/section/div/div/div[1]/div/div/div/article/div/div/div/div[2]/div[2]/div[2]/div[2]/div/span"
 class Tweet:
     def __init__(self,imgHtml:str,timeHtml:str,contentHtml:str,tweetedByHtml:str,href:str):
         self.imgHtml = imgHtml
@@ -42,10 +41,15 @@ def getRecentTweets(twitterHandle: str):
     
     # tweets = browser.find_element_by_xpath(XPATH_OF_RECENT_TWEETS)
     try:
-        WebDriverWait(browser, 30).until( EC.element_to_be_clickable((By.XPATH, XPATH_IMG)))
+        element=WebDriverWait(browser, 300).until( EC.presence_of_element_located((By.XPATH, XPATH_IMG)))
+       
+        print(element.text)
         img = browser.find_element_by_xpath(XPATH_IMG)
+        element=WebDriverWait(browser, 300).until( EC.presence_of_element_located((By.XPATH, XPATH_TIME)))
+        
         time =browser.find_element_by_xpath(XPATH_TIME).text
-        content = browser.find_element_by_xpath(XPATH_CONTENT).get_attribute('outerHTML')
+        element=WebDriverWait(browser, 30).until( EC.presence_of_element_located((By.XPATH, XPATH_CONTENT)))
+        content = browser.find_element_by_xpath(XPATH_CONTENT).text
         tweetedBy=browser.find_element_by_xpath(XPATH_TWEETED_BY).text 
         href=browser.find_element_by_xpath(XPATH_HREF).get_attribute('href')
         res = Tweet(
@@ -55,30 +59,16 @@ def getRecentTweets(twitterHandle: str):
             href=href,
             tweetedByHtml=tweetedBy 
         )
-        browser.close()
-
         return res.to_json()
     except TimeoutException as e:
         print("Wait Timed out")
         print(str(e))
-        browser.close()
-        return {}
-    except Exception as e:
-        browser.close()
-        print("Something went wrong")
-        print(str(e))
-        browser.close()
-        return {}
+        res = {}
+        
+
 
 
 
 if __name__=="__main__":
-    s=getRecentTweets("DarrellMello")
-    print("/***************************/")
-    print(s)
-    print("/***************************/")
-
-    s=getRecentTweets("drbeen_medical")
-    print("/***************************/")
-    print(s)
-    print("/***************************/")
+    t=getRecentTweets("DarrellMello")
+    print(t)
